a146dfb750d48d4e52eb44b4be25b07a7ab820a1
Justin Yum <justinyum98@gmail.com>
2020-06-18 16:58:46 -0700

    modified npm scripts, can set variables for test and dev env

--- src/utils/secrets.ts
+++ src/utils/secrets.ts
@@ -2,37 +2,34 @@ import logger from './logger';
 import dotenv from 'dotenv';
 import fs from 'fs';
 
-if (fs.existsSync('.env.test')) {
-    logger.debug('Using .env.test file to supply config environment variables');
-    dotenv.config({ path: '.env.test' });
-}
-
 export const ENVIRONMENT = process.env.NODE_ENV;
-const prod = ENVIRONMENT === 'production'; // Anything else is treated as 'test'
+const prod = ENVIRONMENT === 'production';
+const dev = ENVIRONMENT === 'dev';
 const test = ENVIRONMENT === 'test';
 
-export const MONGODB_URI = prod ? process.env['MONGODB_URI'] : process.env['MONGODB_URI_TEST'];
+if (!prod) {
+    if (dev && fs.existsSync('.env.dev')) {
+        logger.debug('Dev environment: Loading .env.dev file for env variables');
+        dotenv.config({ path: '.env.dev' });
+    } else if (test && fs.existsSync('.env.test')) {
+        logger.debug('Test environment: Loading .env.test file for env variables');
+        dotenv.config({ path: '.env.test' });
+    } else {
+        logger.error('Error loading environment variables.');
+        process.exit(1);
+    }
+}
 
-export const REDIS_URL = prod ? process.env['REDIS_URL'] : process.env['REDIS_URL_TEST'];
+export const MONGODB_URI = process.env['MONGODB_URI'];
+
+export const REDIS_URL = process.env['REDIS_URL'];
 
 if (!MONGODB_URI) {
-    if (prod) {
-        logger.error('No mongo connection string. Set MONGODB_URI environment variable.');
-    } else if (test) {
-        logger.error('No mongo connection string. Set MONGODB_URI_TEST environment variable.');
-    } else {
-        logger.error('No mongo connection string. Neither in prod or test environment.');
-    }
+    logger.error('No mongo connection string. Set MONGODB_URI environment variable.');
     process.exit(1);
 }
 
 if (!REDIS_URL) {
-    if (prod) {
-        logger.error('No redis connection string. Set REDIS_URL environment variable.');
-    } else if (test) {
-        logger.error('No redis connection string. Set REDIS_URL_TEST environment variable.');
-    } else {
-        logger.error('No redis connection string. Neither in prod or test environment.');
-    }
+    logger.error('No redis connection string. Set REDIS_URL environment variable.');
     process.exit(1);
 }

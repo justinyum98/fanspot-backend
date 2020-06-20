27865b2a90b2bbbdc6ec392319b50166285ad1db
Justin Yum <justinyum98@gmail.com>
2020-06-20 02:33:46 -0700

    wrote test

--- test/integration/follow.test.ts
+++ test/integration/follow.test.ts
@@ -10,79 +10,11 @@ import { closeRedis } from '../../src/redis/actions';
 import { generateJWT } from '../../src/utils/jwt';
 import { createUser, findUserById } from '../../src/database/dataAccess/User';
 
-const FOLLOW_USER = gql`
-    mutation FollowUser($targetUserId: String!) {
-        follow(targetUserId: $targetUserId) {
-            currentUser {
-                id
-                username
-                email
-                following {
-                    id
-                }
-                followers {
-                    id
-                }
-            }
-            targetUser {
-                id
-                username
-                email
-                following {
-                    id
-                }
-                followers {
-                    id
-                }
-            }
-        }
-    }
-`;
-
-const UNFOLLOW_USER = gql`
-    mutation UnfollowUser($targetUserId: String!) {
-        unfollow(targetUserId: $targetUserId) {
-            currentUser {
-                id
-                username
-                email
-                following {
-                    id
-                }
-                followers {
-                    id
-                }
-            }
-            targetUser {
-                id
-                username
-                email
-                following {
-                    id
-                }
-                followers {
-                    id
-                }
-            }
-        }
-    }
-`;
-
 describe('Follow feature', () => {
     let connection: mongoose.Connection;
-    let server: ApolloServer;
-    let client: ApolloServerTestClient;
-    let currentUser: UserDocument;
-    let targetUser: UserDocument;
 
     beforeAll(async () => {
         connection = await connectDatabase();
-        currentUser = await createUser(faker.internet.userName(), faker.internet.password(), faker.internet.email());
-        targetUser = await createUser(faker.internet.userName(), faker.internet.password(), faker.internet.email());
-        const token = generateJWT(currentUser.id, currentUser.username);
-        const context = { token };
-        server = createTestServer(context);
-        client = createTestClient(server);
     });
 
     afterAll(async () => {
@@ -91,10 +23,90 @@ describe('Follow feature', () => {
         await closeDatabase(connection);
     });
 
-    describe('Follow', () => {
+    describe('Mutation Follow', () => {
+        let server: ApolloServer;
+        let client: ApolloServerTestClient;
+        let currentUser: UserDocument;
+        let targetUser: UserDocument;
+
+        const FOLLOW_USER = gql`
+            mutation FollowUser($targetUserId: String!) {
+                follow(targetUserId: $targetUserId) {
+                    currentUser {
+                        id
+                        username
+                        email
+                        following {
+                            id
+                        }
+                        followers {
+                            id
+                        }
+                    }
+                    targetUser {
+                        id
+                        username
+                        email
+                        following {
+                            id
+                        }
+                        followers {
+                            id
+                        }
+                    }
+                }
+            }
+        `;
+
+        const UNFOLLOW_USER = gql`
+            mutation UnfollowUser($targetUserId: String!) {
+                unfollow(targetUserId: $targetUserId) {
+                    currentUser {
+                        id
+                        username
+                        email
+                        following {
+                            id
+                        }
+                        followers {
+                            id
+                        }
+                    }
+                    targetUser {
+                        id
+                        username
+                        email
+                        following {
+                            id
+                        }
+                        followers {
+                            id
+                        }
+                    }
+                }
+            }
+        `;
+
+        beforeAll(async () => {
+            currentUser = await createUser(
+                faker.internet.userName(),
+                faker.internet.password(),
+                faker.internet.email(),
+            );
+            targetUser = await createUser(faker.internet.userName(), faker.internet.password(), faker.internet.email());
+            const token = generateJWT(currentUser.id, currentUser.username);
+            const context = { token };
+            server = createTestServer(context);
+            client = createTestClient(server);
+        });
+
         beforeEach(async () => {
             currentUser = await findUserById(currentUser.id);
             targetUser = await findUserById(targetUser.id);
+        });
+
+        afterAll(async () => {
+            await connection.dropDatabase();
         });
 
         it('can follow another user', async () => {
@@ -201,6 +213,94 @@ describe('Follow feature', () => {
             expect(res.data.unfollow).toBeNull();
             expect(res.errors).toBeDefined();
             expect(res.errors[0].message).toEqual('Already not following user.');
+        });
+    });
+
+    describe('Query Follow', () => {
+        let server: ApolloServer;
+        let client: ApolloServerTestClient;
+        let currentUser: UserDocument;
+        let targetUser: UserDocument;
+
+        const GET_CURRENT_USER_FOLLOWING = gql`
+            query GetCurrentUserFollowing {
+                getCurrentUserFollowing {
+                    user {
+                        id
+                        username
+                        email
+                        isArtist
+                        following {
+                            id
+                            username
+                            email
+                            isArtist
+                        }
+                    }
+                }
+            }
+        `;
+
+        const GET_CURRENT_USER_FOLLOWERS = gql`
+            query GetCurrentUserFollowers {
+                getCurrentUserFollowers {
+                    user {
+                        id
+                        username
+                        followers {
+                            id
+                            username
+                        }
+                    }
+                }
+            }
+        `;
+
+        beforeAll(async () => {
+            currentUser = await createUser(
+                faker.internet.userName(),
+                faker.internet.password(),
+                faker.internet.email(),
+            );
+            targetUser = await createUser(faker.internet.userName(), faker.internet.password(), faker.internet.email());
+            const token = generateJWT(currentUser.id, currentUser.username);
+            server = createTestServer({ token });
+            client = createTestClient(server);
+        });
+
+        afterAll(async () => {
+            await connection.dropDatabase();
+        });
+
+        it("can get a user's list of followers", async () => {
+            // Make targetUser follow currentUser
+            targetUser.following.push(currentUser.id);
+            await targetUser.save();
+            // Update currentUser's followers
+            currentUser.followers.push(targetUser.id);
+            await currentUser.save();
+            const expectedPayload = {
+                user: {
+                    id: currentUser.id,
+                    username: currentUser.username,
+                    followers: [
+                        {
+                            id: targetUser.id,
+                            username: targetUser.username,
+                            email: targetUser.email,
+                            isArtist: targetUser.isArtist,
+                        },
+                    ],
+                },
+            };
+
+            // Retreive currentUser's followers
+            const res = await client.query({
+                query: GET_CURRENT_USER_FOLLOWERS,
+            });
+            const payload = res.data.getCurrentUserFollowers;
+
+            expect(payload).toEqual(expectedPayload);
         });
     });
 });
